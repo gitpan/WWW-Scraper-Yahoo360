2db18d8f9dc54979302edd4cd265cdc94efee66d
Cosimo Streppone <cosimo@cpan.org>
2009-04-27 19:29:39 +0000

    Import of COSIMO/WWW-Scraper-Yahoo360-0.02 from CPAN.
    gitpan-cpan-distribution: WWW-Scraper-Yahoo360
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         COSIMO/WWW-Scraper-Yahoo360-0.02.tar.gz
    gitpan-cpan-author:       COSIMO
    gitpan-cpan-maturity:     released

--- lib/WWW/Scraper/Yahoo360.pm
+++ lib/WWW/Scraper/Yahoo360.pm
@@ -15,11 +15,11 @@ use HTTP::Date     ();
 use JSON::XS       ();
 use WWW::Mechanize ();
 
-use constant BLOG_URL   => 'http://blog.360.yahoo.com/blog/';
-use constant LOGIN_FORM => 'login_form';
+use constant BLOG_URL   => q{http://blog.360.yahoo.com/blog/};
+use constant LOGIN_FORM => q{login_form};
 use constant LOGIN_URL  => q{https://login.yahoo.com/config/login_verify2?.intl=us&.done=http%3A%2F%2Fblog.360.yahoo.com%2Fblog%2F%3F.login%3D1&.src=360};
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 sub new {
     my ($class, $args) = @_;
@@ -28,6 +28,7 @@ sub new {
     bless $self, $class;
 }
 
+# Fetches high-level blog information
 sub blog_info {
     my ($self, $blog_page) = @_;
 
@@ -84,6 +85,7 @@ sub blog_info {
 
 }
 
+# Fetches the user's main blog page
 sub blog_main_page {
     my ($self) = @_;
     my $mech = $self->mech();
@@ -94,6 +96,7 @@ sub blog_main_page {
     Carp::croak("Failed to retrieve blog main page");
 }
 
+# Builds the url to fetch a specific blog page
 sub blog_page_url {
     my ($self, $link, $start, $per_page, $count) = @_;
     my $url = $link;
@@ -106,6 +109,7 @@ sub blog_page_url {
     return $url;
 }
 
+# Logs in to Yahoo
 sub login {
     my ($self) = @_;
 
@@ -129,11 +133,13 @@ sub login {
     return $mech->success();
 }
 
+# Dumps last accessed page content to STDOUT
 sub dump {
     my ($self) = @_;
     print $self->mech->content();
 }
 
+# Retrieves all comments in the user's blog
 sub get_blog_comments {
     my ($self, $posts) = @_;
 
@@ -161,6 +167,7 @@ sub get_blog_comments {
     return \@comments;
 }
 
+# Retrieves all comments to a single blog post
 sub get_blogpost_comments {
     my ($self, $post, $page) = @_;
 
@@ -208,6 +215,7 @@ sub get_blogpost_comments {
     return \@comments;
 }
 
+# Gets all blog posts by a user
 sub get_blog_posts {
     my ($self, $blog_page) = @_;
 
@@ -295,6 +303,7 @@ sub get_blog_posts {
 
 }
 
+# Mechanize object accessor
 sub mech {
     my ($self) = @_;
     if (! exists $self->{_mech}) {
@@ -303,6 +312,7 @@ sub mech {
     return $self->{_mech};
 }
 
+# Tries to parse a date in the Yahoo 360 format
 sub parse_date {
     my ($self, $date) = @_;
 
@@ -344,11 +354,9 @@ sub parse_date {
 
 __END__
 
-# Below is stub documentation for your module. You'd better edit it!
-
 =head1 NAME
 
-WWW::Scraper::Yahoo360 - Perl extension for blah blah blah
+WWW::Scraper::Yahoo360 - Yahoo 360 blogs old-fashioned crappy scraper
 
 =head1 SYNOPSIS
 
@@ -356,32 +364,120 @@ WWW::Scraper::Yahoo360 - Perl extension for blah blah blah
 
   my $y360 = WWW::Scraper::Yahoo360->new({
       username => 'myusername',
-      password => 'password',
+      password => 'mypassword',
   });
 
+  # High level blog information
   my $blog_info = $y360->blog_info();
+
+  # Gets all the blog posts
   my $posts     = $y360->get_blog_posts();
 
+  # Gets all the blog post comments
   my $comments  = $y360->get_blog_comments();
 
 =head1 DESCRIPTION
 
 Ignorant web scraper, based on WWW::Mechanize, that connects to your
 Yahoo 360 account and tries to fetch the blog posts and comments 
-you had on their service.
+you still have on their service.
 
 If it breaks, well... it's a scraper.
 
 This module is used on the My Opera Community, L<http://my.opera.com>,
 to import Yahoo 360 existing blogs into My Opera blog service.
 
-=head2 EXPORT
+=head1 SUBROUTINES
+
+=head2 C<new(\%args)>
+
+Where C<\%args> is a hashref with C<username> and C<password> of your
+B<Yahoo 360> account.
+
+This creates a new C<WWW::Scraper::Yahoo360> object, ready to scrape.
+
+=head2 C<blog_info()>
+
+Fetches high-level blog information for your Yahoo 360 blog.
+Returns a hashref with the some/all the following information:
+    
+=over 4
+
+=item C<link>
+
+Something like: C<<< http://blog.360.yahoo.com/blog-<yourusername> >>>
+
+=item C<sharing>
+
+Most probably C<public>. Could also be C<friends> or C<friends of friends>,
+but never tried it.
+
+=item C<count>
+
+Number of blog posts in total.
+
+=item C<start>
+
+First blog post on the frontpage. Should be 1.
+
+=item C<end>
+
+Last blog post on the frontpage, usually 5.
+
+=item C<title>
+
+Title of the blog.
+
+=back
+
+=head2 C<blog_main_page()>
+
+Fetches the user's main blog page.
+Returns a string with the HTML page contents.
+
+=head2 C<blog_page_url($link, $start, $per_page, $count)>
+
+Builds the url to fetch a specific blog page.
+
+=head2 C<login()>
+
+Logs in to Yahoo service.
+
+=head2 C<dump()>
+
+Dumps last accessed page content to STDOUT
+
+=head2 C<get_blog_comments(\@posts)>
+
+Retrieves all comments in the user's blog.
+Wants the structure returned by C<get_blog_posts()>.
+
+=head2 C<get_blogpost_comments($post)>
+
+Retrieves all comments to a single blog post.
+Wants a single C<$post> entry (hashref): one of the elements
+returned by C<get_blog_posts()>.
+
+=head2 C<get_blog_posts()>
+
+Gets all blog posts by a user.
+Returns an array of hashrefs, each one representing a blog post.
+
+=head2 C<mech()>
+
+C<WWW::Mechanize> object accessor.
+
+=head2 C<parse_date($date_string)>
+
+Tries to parse a date from the Yahoo 360 format to a unix timestamp.
+
+=head1 EXPORTS
 
 None by default.
 
 =head1 AUTHOR
 
-cosimo, E<lt>cosimo@cpan.orgE<gt>
+Cosimo Streppone, E<lt>cosimo@cpan.orgE<gt>
 
 =head1 COPYRIGHT AND LICENSE
 
@@ -392,3 +488,4 @@ it under the same terms as Perl itself, either Perl version 5.8.8 or,
 at your option, any later version of Perl 5 you may have available.
 
 =cut
+
